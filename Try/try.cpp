8ed49a644290f6ab09a10f727c7947257ca89b6b
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-06-06 19:11:40 +0800

    201806061911

--- Try/try.cpp
+++ Try/try.cpp
@@ -3,28 +3,81 @@
 #include<fstream>
 #include<stdexcept>
 #include<exception>
+class C
+{
+public:
+	C();
+private:
+	static double cDblA;
+};
+C::C()
+{
+	std::cout << cDblA;
+}
+double C::cDblA = 223.34;
+template<typename T>
+class B;
+int fun()
+{
+	return 0;
+}
 void terminate_handle()
 {
 	std::cerr << "Terminalte.\n;";
 }
+template<typename T>
+class A;
+template <typename T=double>
 class A
 {
 public:
 	A();
 	~A();
 	explicit A(int);
+	friend class c;
+	friend  B<double>;
+	friend int fun();
 	int data;
 private:
+	T temp;
+	const char cChr = 'a';
+	const static double csChrA;
+};
+template <typename T>
+const double A<T>::csChrA = 'a';
+template<typename T>
+A<T>::A()
+{
+	std::cout << cChr<<std::endl;
+	std::cout << A::csChrA;
+}
+template<typename T>
+A<T>::~A()
+{
+}
+template <>
+class A<int>
+{
+public:
+	A();
+	~A();
+	int data;
+
+private:
+
 };
 
-A::A()
+A<int>::A()
 {
 }
 
-A::~A()
+
+A<int>::~A()
 {
 }
-class B :public A
+
+template<typename T>
+class B :public A<T>
 {
 public:
 	B();
@@ -34,24 +87,41 @@ public:
 private:
 
 };
+template<typename T>
 
-B::B()
+B<T>::B()
 {
 }
+template<typename T>
 
-B::B(int a)
+B<T>::B(int a)
 {
-	A::data = a;
+	A<int>::data = a;
 
 }
-B::~B()
+template<typename T>
+B<T>::~B()
 {
 }
+template <typename T>
+T add(T a, T b)
+{
+	std::cout << "Template called.\n";
+	return a + b;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
 	using std::fstream;
 	using std::cout;
 	using std::cin;
+	C aaa;
 	//cout << std::right << std::setw(10) << 'T';
 	//fstream a;
 	//a.open("a.txt",std::ios::out|std::ios::in/*|std::ios::app*/);
@@ -67,18 +137,23 @@ int main()
 	//char * b=new char[100];
 	//a >> b;
 	//cout << std::endl<<b;
+	int * intArr = new int[10];
+	intArr++;
+	delete --intArr;
+	add(2, 3);
 	std::set_new_handler(terminate_handle);
 	std::exception expA{"expAAA"};
 	//cout << expA.what();
 	std::runtime_error::exception rteA;
 	set_terminate(terminate_handle);
+	A<double> dblAa;
 	try
 	{
-		throw(B(1));
+		throw(B<int>(1));
 		throw(expA);
 		
 	}
-	catch (A  & a)
+	catch (A<int>  & a)
 	{
 		std::cout << "A called\n";
 	}
@@ -86,6 +161,5 @@ int main()
 	{
 		std::cout << exp.what();
 	}
-	
 	return 0;
 }
