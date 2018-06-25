e0b24d84d1efc437c939eb27796288d17d8d8ee5
Hakurei Reimu <Hakurei__Reimu@Outlook.com>
2018-06-25 16:37:53 +0800

    201807261637

--- a.cpp
+++ a.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
  int main()
 {
+	int a =0;
+	std::cout<<"Input the value to test gdb's value function:";
+	std::cin>>a;
+	if(a>20)
+	{
+		std::cout<<"a >20";
+	}
+	else
+	{
+		std::cout<<"a<=20";
+	}
+	//Test commit
 	std::cout<<"hello world!\n";
 	return 0;
 }
