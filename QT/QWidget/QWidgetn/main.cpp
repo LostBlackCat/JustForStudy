52c0d5e623c5751b64003b43be2c756b3d44ef4e
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-07-03 07:43:53 +0800

    201807030744

--- QT/QWidget/QWidgetn/main.cpp
+++ QT/QWidget/QWidgetn/main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
+#include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  MainWindow w;
-  w.setGeometry(100,100,200,120);
+  Widget w;
   w.show();
 
   return a.exec();
