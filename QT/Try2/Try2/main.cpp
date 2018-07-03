4c56b1e0852c7836102fc8b34392c16569ef5b33
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-07-03 11:30:53 +0800

    201807031130

--- QT/Try2/Try2/main.cpp
+++ QT/Try2/Try2/main.cpp
@@ -1,16 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include<QPushButton>
+#include<QHBoxLayout>
+#include<QLineEdit>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
   QWidget * window=new QWidget();
+  QPushButton *button=new QPushButton("Press me ",window);
+  QHBoxLayout layout(window);
+  QLineEdit *lineEdit=new QLineEdit("Line edit",window);
+
   window->resize(320,240);
   w.setGeometry(100,100,200,120);
+  layout.addWidget(button);
+  layout.addWidget(lineEdit);
   w.show();
-  window->show();
+  button->move(100,100);
   window->setWindowTitle("Doubel Window");
-  w.show();
+  button->show();
+  window->show();
+  w.close();
   return a.exec();
 }
