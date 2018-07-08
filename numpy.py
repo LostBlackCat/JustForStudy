87d200e95283a09fd4280d04ee691895b07e7b1f
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-07-08 22:39:30 +0800

    201807082239

--- numpy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import time
-a=np.random.rand(1000000)
-b=np.random.rand(1000000)
-
-tic=time.time()
-c=np.dot(a,b)
-toc=time.time()
-print("Cactorized version:"+str(1000*(toc-tic)))
